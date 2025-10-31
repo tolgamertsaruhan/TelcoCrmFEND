5215117df65b3323ad9a5aee70ef17cc5946134f
Tolga Mert Saruhan (ETIYA) <tolga.saruhan@etiya.com>
2025-10-31 16:24:15 +0300

    customer-information-screen

---
+++ src/app/models/individualcustomer/responses/CreateFullIndividualCustomerResponse.ts
@@ -0,0 +1,35 @@
+export interface CreatedIndividualCustomerResponse {
+  id: string;
+  firstName: string;
+  lastName: string;
+  middleName?: string;
+  nationalId: string;
+  motherName?: string;
+  fatherName?: string;
+  dateOfBirth?: string;
+  gender?: string;
+}
+ 
+export interface CreatedAddressResponse {
+  id: string;
+  street: string;
+  houseNumber?: string;
+  description?: string;
+  isDefault: boolean;
+  districtId: string;
+  customerId: string;
+}
+ 
+export interface CreatedContactMediumResponse {
+  id: string;
+  customerId: string;
+  type: string;
+  value: string;
+  isPrimary: boolean;
+}
+ 
+export interface CreateFullIndividualCustomerResponse {
+  customerResponse: CreatedIndividualCustomerResponse;
+  addressResponse: CreatedAddressResponse[];
+  createdContactMediumResponses: CreatedContactMediumResponse[];
+}
---
+++ src/app/pages/customer-information-screen/customer-information-screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-information-screen',
+  imports: [],
+  templateUrl: './customer-information-screen.html',
+  styleUrl: './customer-information-screen.scss',
+})
+export class CustomerInformationScreen {
+
+}

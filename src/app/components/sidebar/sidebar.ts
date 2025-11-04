6e40965dbe6f6e6915b84963c9ea37cde4d0dd10
sena.eraydin <senaeraydin2@gmail.com>
2025-11-04 20:28:33 +0300

    sidebar fixed - sidebar exit fixed

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule,
+    RouterLink,
+    RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  constructor(public authService: AuthService) {}
+   isOpen = signal(true);
 
+  onToggle() {
+    this.isOpen.set(!this.isOpen());
+  }
 }

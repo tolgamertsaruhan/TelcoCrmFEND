222965f43af49055b81b45a98e6c5de8c77ccca1
sena.eraydin <senaeraydin2@gmail.com>
2025-11-12 17:34:18 +0300

    fr1  - 1 test case finished

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -19,9 +19,9 @@ export class Login {
   errorMessage: string | null = null;
   showPassword = false;
 
-  private emailRegex = /^[a-z]+\.[a-z]+@etiya\.com$/; // ETIYA_EMAIL_REGEX
+  //private emailRegex = /^[a-z]+\.[a-z]+@etiya\.com$/; // ETIYA_EMAIL_REGEX
   
-  private passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-\=\[\]{};':"\\|,.<>/?]).{8,}$/; // STRONG_PASSWORD_REGEX
+  //private passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-\=\[\]{};':"\\|,.<>/?]).{8,}$/; // STRONG_PASSWORD_REGEX
 
   constructor(private fb: FormBuilder, private auth: AuthService, private router: Router, private cd: ChangeDetectorRef) {}
 
@@ -34,8 +34,8 @@ export class Login {
     }
 
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.pattern(this.emailRegex)]],
-      password: ['', [Validators.required, Validators.pattern(this.passwordRegex)]]
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
 
